8c2d4be1e200ba9c150c9c7b1e3971c73e4dace2
KAPIL <kapilmali7789@gmail.com>
2026-06-19 08:55:00 +0530

    Time: 0 ms (100%), Space: 7.9 MB (48.9%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        if(n == 1)return true;
+
+        long long power = 2;
+
+        while(power <= n){
+            if(power == n){
+                return true;
+            }
+            power = power*2;
+        }
+        return false;
+    }
+};
